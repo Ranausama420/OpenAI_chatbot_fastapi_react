45e7a794547ac06c7f4dd26bde9cd4e2cd970b13
Ranausama420 <43530464+Ranausama420@users.noreply.github.com>
2023-06-05 21:57:52 +0500

    Initial commit

--- backend/functions/openai_requests.py
+++ backend/functions/openai_requests.py
@@ -1,5 +1,5 @@
 import openai
-from PyPDF2 import PdfReader
+# from PyPDF2 import PdfReader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
